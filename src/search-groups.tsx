3efcb4bc2448d8eafb54f1d4a94b4b36f972955c
Joey Chai <crazyczy@live.cn>
2023-04-07 23:30:34 +0800

    feat(group): search groups

--- src/search-groups.tsx
+++ src/search-groups.tsx
@@ -3,24 +3,21 @@ import { useFetch } from '@raycast/utils';
 import { useState } from 'react';
 import * as cheerio from 'cheerio';
 
-type Music = {
+type group = {
   category: string;
   title: string;
-  year: string;
-  rating: string;
-  singers: string[];
   url: string;
   cover: string;
-  typ: string;
+  member: string;
 };
 
-type SearchResult = Music[];
+type SearchResult = group[];
 
 export default function Command() {
   const [search, setSearch] = useState<string>('');
   const [showingDetail, setShowingDetail] = useState(true);
 
-  const { data, isLoading } = useFetch(`https://www.douban.com/search?q=${search}&cat=1003`, {
+  const { data, isLoading } = useFetch(`https://www.douban.com/search?q=${search}&cat=1019`, {
     execute: search.trim().length > 0,
     headers: {
       'User-Agent':
@@ -31,7 +28,7 @@ export default function Command() {
         throw new Error(response.statusText);
       }
 
-      const musicList: SearchResult = [];
+      const groups: SearchResult = [];
       const data = await response.text();
       if (data) {
         const $ = cheerio.load(data);
@@ -41,44 +38,29 @@ export default function Command() {
           const category = $(item).find('h3 span:first')?.text()?.trim() || '';
           const url = $(item).find('div.content a')?.prop('href')?.trim() || '';
           const title = $(item).find('div.title a')?.text()?.trim() || '';
-          const rating = $(item).find('span.rating_nums')?.text()?.trim() || '';
-          const subjectEl = $(item).find('span.subject-cast')?.text()?.split('/');
-          const year = subjectEl?.pop()?.trim() || '';
-          const typ = subjectEl?.pop()?.trim() || '';
-          const singers = $(item)
-            .find('span.subject-cast')
-            ?.text()
-            ?.split('/')
-            .slice(0, -2)
-            .map((singer) => singer.trim()) || [''];
           const cover = $(item).find("img[src^='https']").attr('src') || '';
+          const member = $(item).find('div.info')?.text()?.trim() || '';
 
-          const music: Music = {
+          const group: group = {
             category,
             title,
-            rating,
-            year,
-            singers,
             url,
             cover,
-            typ,
+            member,
           };
 
-          musicList.push(music);
+          groups.push(group);
         });
       }
 
-      return musicList;
+      return groups;
     },
   });
 
-  function metadata(music: Music) {
+  function metadata(group: group) {
     return (
       <List.Item.Detail.Metadata>
-        <List.Item.Detail.Metadata.Label title="Year" text={music.year} />
-        <List.Item.Detail.Metadata.Label title="Rating" text={music.rating} />
-        <List.Item.Detail.Metadata.Label title="Singers" text={music.singers && music.singers.join(' / ')} />
-        <List.Item.Detail.Metadata.Label title="Type" text={music.typ} />
+        <List.Item.Detail.Metadata.Label title="Member" text={group.member} />
       </List.Item.Detail.Metadata>
     );
   }
@@ -88,28 +70,28 @@ export default function Command() {
       isShowingDetail={true}
       isLoading={isLoading}
       throttle={true}
-      searchBarPlaceholder="Search Music on Douban"
+      searchBarPlaceholder="Search Groups on Douban"
       onSearchTextChange={(newValue) => setSearch(newValue)}
     >
       {search === '' ? (
         <List.EmptyView />
       ) : (
         data &&
-        data.map((music) => (
+        data.map((group) => (
           <List.Item
-            key={music.url}
-            title={music.title}
-            subtitle={music.category}
-            icon={{ source: music.cover }}
+            key={group.url}
+            title={group.title}
+            subtitle={group.category}
+            icon={{ source: group.cover }}
             detail={
               <List.Item.Detail
-                markdown={`![Illustration](${music.cover})`}
-                metadata={showingDetail ? metadata(music) : ''}
+                markdown={`![Illustration](${group.cover})`}
+                metadata={showingDetail ? metadata(group) : ''}
               />
             }
             actions={
               <ActionPanel>
-                <Action.OpenInBrowser url={music.url} />
+                <Action.OpenInBrowser url={group.url} />
                 <Action title="Toggle Detail" onAction={() => setShowingDetail(!showingDetail)} />
               </ActionPanel>
             }
